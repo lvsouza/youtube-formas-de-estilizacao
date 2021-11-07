79106973b1347b2c4c1f27ca51312aa738fb9e6a
lvsouza <ls18017@gmail.com>
2021-11-07 19:39:11 -0300

    Aula 03: Usando o jss (css in js)

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,23 @@
-import './Dashboard.css';
+import { createUseStyles } from 'react-jss';
+
+const useClasses = createUseStyles({
+  paragraph: {
+    backgroundColor: 'black',
+    color: 'white',
+
+    '&:hover': {
+      backgroundColor: 'red',
+    },
+    '&:active': {
+      backgroundColor: 'blue',
+    }
+  },
+});
 
 export const Dashboard = () => {
+  const classes = useClasses();
 
   return (
-    <p className="background-color">Teste</p>
+    <p className={classes.paragraph}>Teste</p>
   );
 }
