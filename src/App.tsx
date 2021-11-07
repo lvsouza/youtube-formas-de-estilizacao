00b59583bb64bc1f08bd5855f8c8832509f9599c
lvsouza <ls18017@gmail.com>
2021-11-07 09:19:42 -0300

    Aula 01: CSS inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+export const App = () => {
+  const [padding, setPadding] = useState(0)
+
+  useEffect(() => {
+    /* setInterval(() => {
+      setPadding(oldPadding => {
+        return oldPadding + 1
+      })
+    }, 2000); */
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'black', color: 'white', padding: padding }}>
+      Teste
     </div>
   );
 }
-
-export default App;
